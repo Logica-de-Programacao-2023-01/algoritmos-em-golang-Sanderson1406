8aab16e734fdb4d9ce71c1965631216e328fdf51
Sanderson Oliveira <84995084+Sanderson1406@users.noreply.github.com>
2023-03-20 20:18:48 -0300

    Update 2_6.go

--- 2_6.go
+++ 2_6.go
@@ -3,17 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var n1 uint64
-	var n2 uint64
+	var n1 int64
+	var n2 int64
 	fmt.Println("1º Termo: ")
 	fmt.Scan(&n1)
 	fmt.Println("2º Termo: ")
 	fmt.Scan(&n2)
-	if n1 < 0 && n2 < 0 {
+	if n1 < 1 && n2 < 1 {
 		fmt.Println("O resultado da multiplicação desses termos é: ", n1*n2)
-	} else if n1 > 0 {
-		fmt.Println("O resultado da soma desses termos é: ", n1-n2)
-	} else if n2 > 0 {
+	} else if n1 < -1 || n2 < -1 {
 		fmt.Println("O resultado da soma desses termos é: ", n1-n2)
 	}
 }
