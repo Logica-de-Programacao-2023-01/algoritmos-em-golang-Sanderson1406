ad225f4c47afabf00aa9488edec864e7bc339515
Sanderson Oliveira <84995084+Sanderson1406@users.noreply.github.com>
2023-03-20 19:58:44 -0300

    Add files via upload

---
+++ 2_5.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n1 uint64
+	fmt.Println("Digite um número: ")
+	fmt.Scan(&n1)
+	if n1%3 == 0 && n1%5 == 0 {
+		fmt.Println("O número é multiplo de 3 e 5 ao mesmo tempo")
+	} else {
+		fmt.Println("Nãp é multiplo de 3 e 5 ao mesmo tempo")
+	}
+}
