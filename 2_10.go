2a14845211e41416d7e0f110866cc3362e50bc57
Sanderson Oliveira <84995084+Sanderson1406@users.noreply.github.com>
2023-03-20 20:35:41 -0300

    Add files via upload

---
+++ 2_10.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var id uint32
+	fmt.Println("Qual a sua idade?")
+	fmt.Scan(&id)
+	if id <= 9 {
+		fmt.Println("Você é um Mirim")
+	} else if id >= 10 && id <= 13 {
+		fmt.Println("Você é Infantil")
+	} else if id >= 14 && id <= 17 {
+		fmt.Println("Você é um Juvenil")
+	} else if id > 18 {
+		fmt.Println("Você é um Adulto")
+	}
+}
