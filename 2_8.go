458d6c60723cf720988f307fe64e4f70c05da23e
Sanderson Oliveira <84995084+Sanderson1406@users.noreply.github.com>
2023-03-20 20:31:04 -0300

    Add files via upload

---
+++ 2_8.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var dia uint32
+	fmt.Println("Qual a contagem do dia em números? ")
+	fmt.Scan(&dia)
+	if dia == 1 {
+		fmt.Println("O dia é domingo")
+	} else if dia == 2 {
+		fmt.Println("O dia é segunda-feira")
+	} else if dia == 3 {
+		fmt.Println("O dia é terça-feira")
+	} else if dia == 4 {
+		fmt.Println("O dia é quarta-feira")
+	} else if dia == 5 {
+		fmt.Println("O dia é quinta-feira")
+	} else if dia == 6 {
+		fmt.Println("O dia é sexta-feira")
+	} else if dia == 7 {
+		fmt.Println("O dia é sabádo")
+	}
+}
