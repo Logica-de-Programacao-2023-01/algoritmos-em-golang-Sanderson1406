3fb47e3a854cfe3ad371a82063623602ebd4073d
Sanderson Oliveira <84995084+Sanderson1406@users.noreply.github.com>
2023-03-20 17:20:21 -0300

    Add files via upload

---
+++ 10.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var kls float64
+	var libras float64
+	fmt.Println("Insira seu peso em KLs: ")
+	fmt.Scanln(&kls)
+	libras = 2.2
+	fmt.Println("Seu peso em Libras é: ", kls*libras)
+}
