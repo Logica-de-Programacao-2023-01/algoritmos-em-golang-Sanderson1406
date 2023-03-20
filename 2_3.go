49a88040d29a08005c43371c1dbd72d45c0077db
Sanderson Oliveira <84995084+Sanderson1406@users.noreply.github.com>
2023-03-20 17:59:43 -0300

    Update 2_3.go

--- 2_3.go
+++ 2_3.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n uint32
+	fmt.Println("Insira um número: ")
+	fmt.Scanln(&n)
+	if n%2 == 0 {
+		fmt.Println(n, "é par")
+	} else {
+		fmt.Println(n, "é impar")
+	}
 }
