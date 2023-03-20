c0033694a80aed3c313f5d1500a2eb6c470285d1
Sanderson Oliveira <84995084+Sanderson1406@users.noreply.github.com>
2023-03-20 20:02:25 -0300

    Update 2_5.go

--- 2_5.go
+++ 2_5.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	var n1 uint64
 	fmt.Println("Digite um número: ")
-	fmt.Scan(&n1)
-	if n1%3 == 0 && n1%5 == 0 {
+	fmt.Scanln(&n1)
+	if (n1*3 != 0) == (n1*5 != 0) {
 		fmt.Println("O número é multiplo de 3 e 5 ao mesmo tempo")
 	} else {
 		fmt.Println("Nãp é multiplo de 3 e 5 ao mesmo tempo")
