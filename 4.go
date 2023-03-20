0dcb2aafb074595e4b22b6a6b1445a191068cd0f
Sanderson Oliveira <84995084+Sanderson1406@users.noreply.github.com>
2023-03-20 16:01:00 -0300

    Add files via upload

---
+++ 4.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var p1 float64
+	var p2 float64
+	var p3 float64
+	var x1 float64 = 2
+	var x2 float64 = 3
+	var x3 float64 = 5
+	var ele1 float64 = 0
+	var ele2 float64 = 0
+	var ele3 float64 = 0
+	var baixo float64 = 0
+	fmt.Println("Número 1: ")
+	fmt.Scanln(&p1)
+	fmt.Println("Número 2: ")
+	fmt.Scanln(&p2)
+	fmt.Println("Número 3: ")
+	fmt.Scanln(&p3)
+	ele1 = x1 * p1
+	ele2 = x2 * p2
+	ele3 = x3 * p3
+	baixo = p1 + p2 + p3
+	fmt.Println("Sua média ponderada é:", ele1+ele2+ele3/baixo)
+}
