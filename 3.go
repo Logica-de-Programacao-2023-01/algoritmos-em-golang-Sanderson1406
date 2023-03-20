61b02e7d54c680f64c3c1488d16ce7a23948c7cc
Sanderson Oliveira <84995084+Sanderson1406@users.noreply.github.com>
2023-03-20 18:24:21 -0300

    Update 3.go

--- 3.go
+++ 3.go
@@ -5,10 +5,21 @@ import "fmt"
 func main() {
 	var peso float64
 	var altura float64
+	var imc float64
 	fmt.Println("Insira seu peso: ")
 	fmt.Scan(&peso)
 	fmt.Println("Insira seua altura: ")
 	fmt.Scan(&altura)
 	altura *= altura
-	fmt.Println("Seu IMC é: ", peso/altura)
+	imc = peso / altura
+	fmt.Println("Seu IMC é: ", imc)
+	if imc < 18.5 {
+		fmt.Print("Você está abaixo do peso normal")
+	} else if 18.5 <= imc && imc <= 24.9 {
+		fmt.Print("Você está no peso ideal")
+	} else if 25 <= imc && imc <= 29.9 {
+		fmt.Print("Você está com sobrepeso")
+	} else if imc >= 30 {
+		fmt.Print("Você está com obesidade")
+	}
 }
