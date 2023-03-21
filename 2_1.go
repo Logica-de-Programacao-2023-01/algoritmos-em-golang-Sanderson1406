99e672a3733b075cf50102b37139c3dedc92ce65
Sanderson <sandersonoficial10@hotmail.com>
2023-03-21 15:25:25 -0300

    teste

--- 2_1.go
+++ 2_1.go
@@ -9,11 +9,11 @@ func main() {
 	fmt.Scanln(&n1)
 	fmt.Println("Insira outro número: ")
 	fmt.Scanln(&n2)
-	if n1 == n2 {
+	if n1 < n2 {
+		fmt.Println(n2, " é menor")
+	} else if n2 > n1 {
+		fmt.Println(n1, "é menor")
+	} else if n1 == n2 {
 		fmt.Println("O primeiro número é igual ao segundo número")
-	} else if n1 > n2 {
-		fmt.Println("O primeiro número é maior que o segundo número")
-	} else if n1 < n2 {
-		fmt.Println("O primeiro número é menor que segundo número")
 	}
 }
